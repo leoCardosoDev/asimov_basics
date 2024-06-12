8b971c79d37357ff31b6a4759d3692d546f5626f
leoCardosoDev <leocardosodev@gmail.com>
2024-06-12 12:22:43 -0300

    feat: return folder parents

--- 03_lendo_escrevendo_arquivos/5_manipulando _caminho.py
+++ 03_lendo_escrevendo_arquivos/5_manipulando _caminho.py
@@ -27,3 +27,47 @@ print(Path(__file__).is_absolute())
 print(Path(__file__).parent)
 print(Path(__file__).parent / 'assets/primeira_pasta')
 print((Path(__file__).parent / 'assets/primeira_pasta').exists())
+print('\n\n')
+# Trabalhando com partes de caminho
+caminho_arquivo = Path(__file__)
+print('Pasta Atual')
+print(caminho_arquivo)  # pasta atual
+print('================================ \n')
+
+print('Pasta Raiz')
+print(caminho_arquivo.anchor)  # pasta raiz
+print('================================\n')
+
+print('Pasta Anterior')
+print(caminho_arquivo.parent)  # pasta anterior
+print('================================\n')
+
+print('Tipo de caminho (Sistema operacional)')
+print(type(caminho_arquivo.parent))  # tipo
+print('================================\n')
+
+print('Nome do arquivo COM extensão')
+print(caminho_arquivo.name)  # nome
+print('================================\n')
+
+print('Nome do arquivo SEM extensão')
+print(caminho_arquivo.stem)  # nome
+print('================================\n')
+
+print('Obtendo a extensão do arquivo')
+print(caminho_arquivo.suffix)  # extensão do arquivo
+print('================================\n')
+
+print('Nome do disco no Computador')
+print(caminho_arquivo.drive)  # Nome do disco
+print('================================\n')
+
+print('Diferença de PARENT e PARENTS')
+print(caminho_arquivo.parent)  # pasta anterior
+print(caminho_arquivo.parent.parent)  # Modo de acessar a pasta anterior da anterior
+print(caminho_arquivo.parents[0])  # Modo de acessar as pastas anteriores mais rápido
+print(caminho_arquivo.parents[1])  # Modo de acessar as pastas anteriores mais rápido
+print(caminho_arquivo.parents[2])  # Modo de acessar as pastas anteriores mais rápido
+print(caminho_arquivo.parents[3])  # Modo de acessar as pastas anteriores mais rápido
+print(len(caminho_arquivo.parents))
+print('================================\n')
