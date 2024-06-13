0697af99b1841d8c04201e46fe89acb71b5a7d07
leoCardosoDev <leocardosodev@gmail.com>
2024-06-13 07:40:47 -0300

    feat: lista files in folders

---
+++ 03_lendo_escrevendo_arquivos/3_lendo_listando_conteudo_de_pastas.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+import os
+
+# Listando arquivos de uma pasta
+print('LISTANDO ARQUIVOS DE UMA PASTA')
+print(os.listdir(Path.cwd()))  # Listando com os
+print(list(Path.cwd().glob('*')))
+print('================================================================\n')
+print(list(Path.cwd().glob('03_lendo_escrevendo_arquivos/')))
+print('================================================================\n')
+print(list(Path.cwd().glob('03_lendo_escrevendo_arquivos/*')))
+print('================================================================\n')
+print(list(Path.cwd().glob('03_lendo_escrevendo_arquivos/*.py')))
+
+
