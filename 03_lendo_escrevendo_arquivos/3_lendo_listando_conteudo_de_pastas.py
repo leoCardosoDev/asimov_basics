c1d3d8845198866b3bd1eb2275a4a9bdc7102e03
leoCardosoDev <leocardosodev@gmail.com>
2024-06-13 07:49:20 -0300

    feat: list all specific files in folders

--- 03_lendo_escrevendo_arquivos/3_lendo_listando_conteudo_de_pastas.py
+++ 03_lendo_escrevendo_arquivos/3_lendo_listando_conteudo_de_pastas.py
@@ -10,9 +10,13 @@ print(list(Path.cwd().glob('03_lendo_escrevendo_arquivos/')))
 print('================================================================\n')
 print(list(Path.cwd().glob('03_lendo_escrevendo_arquivos/*')))
 print('================================================================\n')
-print(list(Path.cwd().glob('03_lendo_escrevendo_arquivos/*.py')))
 
 # Listando tudo dentro de uma pasta
 print('================================================================\n')
 print('LISTANDO TUDO')
 print(list(Path.cwd().glob('03_lendo_escrevendo_arquivos/**/*')))
+
+# Listando arquivos de uma determinada extensão
+print('================================================================\n')
+print(list(Path.cwd().glob('03_lendo_escrevendo_arquivos/**/*.csv')))
+print('================================================================\n')
