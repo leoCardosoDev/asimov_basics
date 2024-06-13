ebdccfcca67e6b752979f6ea4f232a8290d4cefd
leoCardosoDev <leocardosodev@gmail.com>
2024-06-13 08:59:28 -0300

    feat: desafio 1 - Arquivos

---
+++ 03_lendo_escrevendo_arquivos/5_desafio.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import os
+
+def se_tiver_nomes_especificos(diretorio):
+  if diretorio.is_dir() and not diretorio.name.startswith('.') and not diretorio.name == 'node_modules':
+    return True
+  return False
+  
+# Retorna tamanho dos diretórios
+def retorna_tamanho_arquivos(caminho, profundidade=1, tamanho_linha=0):
+  for diretorio in caminho.glob('*'):
+    if se_tiver_nomes_especificos(diretorio):
+      tamanho_diretorio = 0
+      for arquivo in diretorio.glob('**/*'):
+        if arquivo.is_file():
+          tamanho_diretorio += os.path.getsize(arquivo)
+        print('--' * tamanho_linha, diretorio.name, round(tamanho_diretorio / 1024 / 1024, 2), 'mb')
+        if profundidade > 1:
+          retorna_tamanho_arquivos(diretorio, profundidade-1, tamanho_linha+1)
+
+caminho = Path.home() / 'cursos/python'
+
+retorna_tamanho_arquivos(caminho, profundidade=2)
