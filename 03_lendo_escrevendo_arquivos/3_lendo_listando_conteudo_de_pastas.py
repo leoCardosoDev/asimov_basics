f1dba87384f3d1db82eb68cb12cb0535e92f194b
leoCardosoDev <leocardosodev@gmail.com>
2024-06-13 07:44:53 -0300

    feat: list all files in folders

--- 03_lendo_escrevendo_arquivos/3_lendo_listando_conteudo_de_pastas.py
+++ 03_lendo_escrevendo_arquivos/3_lendo_listando_conteudo_de_pastas.py
@@ -12,4 +12,7 @@ print(list(Path.cwd().glob('03_lendo_escrevendo_arquivos/*')))
 print('================================================================\n')
 print(list(Path.cwd().glob('03_lendo_escrevendo_arquivos/*.py')))
 
-
+# Listando tudo dentro de uma pasta
+print('================================================================\n')
+print('LISTANDO TUDO')
+print(list(Path.cwd().glob('03_lendo_escrevendo_arquivos/**/*')))
