921d854358363a16d768f302dde3e9003c071656
leoCardosoDev <leocardosodev@gmail.com>
2024-06-17 15:02:51 -0300

    feat: copy folders in cascade

--- 05_copiando_e_movendo_pastas/03_copiando_pastas.py
+++ 05_copiando_e_movendo_pastas/03_copiando_pastas.py
@@ -1,3 +1,7 @@
 from pathlib import Path
+import shutil
 
 # Copiando pastas
+pasta_atual = Path(__file__).parent / 'assets' / 'destino'
+shutil.copytree(pasta_atual  / '05', pasta_atual  / '04' / '05' )
+print(pasta_atual)
