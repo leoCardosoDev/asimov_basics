10d09975b91c66ef981b13ef23688ce867cd475d
leoCardosoDev <leocardosodev@gmail.com>
2024-06-17 14:53:10 -0300

    refactor: remove parents true

--- 05_copiando_e_movendo_pastas/01_criando_pastas.py
+++ 05_copiando_e_movendo_pastas/01_criando_pastas.py
@@ -3,5 +3,6 @@ from pathlib import Path
 # Criando pastas
 pasta_atual = Path(__file__).parent
 caminho_pasta_destino = pasta_atual / 'assets/destino/04'
-caminho_pasta_destino.mkdir(parents=True, exist_ok=True)
+# caminho_pasta_destino.mkdir(exist_ok=True)
+caminho_pasta_destino.mkdir(exist_ok=True, parents=True)
 print(pasta_atual)
