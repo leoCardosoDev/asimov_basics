7d0f98e7b3bce72bcb7066954f50ce7aa2d19d45
leoCardosoDev <leocardosodev@gmail.com>
2024-06-17 14:53:42 -0300

    feat: create folders in cascade

--- 05_copiando_e_movendo_pastas/02_criando_pastas_anteriores_necessaria.py
+++ 05_copiando_e_movendo_pastas/02_criando_pastas_anteriores_necessaria.py
@@ -1,3 +1,7 @@
 from pathlib import Path
 
 # Criando pastas com todas as pastas anteriores necessárias
+pasta_atual = Path(__file__).parent
+caminho_pasta_destino = pasta_atual / 'assets/destino/05' / '051'
+caminho_pasta_destino.mkdir(parents=True, exist_ok=True)
+print(pasta_atual)
