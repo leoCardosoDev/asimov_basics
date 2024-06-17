bc67fe29c64e02cadddfde89fe25709e6a7d142b
leoCardosoDev <leocardosodev@gmail.com>
2024-06-17 11:41:26 -0300

    refactor: rename and organize files

---
+++ 04_copiando_e_movendo_arquivos/01_1_copyfile.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+import shutil
+
+# Copiando arquivo com copyfile
+pasta_atual = Path(__file__).parent
+print(pasta_atual)
+caminho_arquivo_origem = pasta_atual / 'assets' / 'texto.txt'
+print(caminho_arquivo_origem)
+caminho_arquivo_destino = pasta_atual / 'assets/destino/01' / 'texto.txt'
+# Garantir que o diretório de destino exista
+# caminho_arquivo_destino.parent.mkdir(parents=True, exist_ok=True)
+# Copiar o arquivo
+shutil.copyfile(caminho_arquivo_origem, caminho_arquivo_destino)
--- 04_copiando_e_movendo_arquivos/01_copiando_e_movendo.py
+++ 04_copiando_e_movendo_arquivos/01_copiando_e_movendo.py
@@ -14,6 +14,15 @@ shutil.copyfile(caminho_arquivo_origem, caminho_arquivo_destino)
 
 
 # Copiando arquivo com copy
+pasta_atual = Path(__file__).parent
+print(pasta_atual)
+caminho_arquivo_origem = pasta_atual / 'assets' / 'texto.txt'
+print(caminho_arquivo_origem)
+caminho_arquivo_destino = pasta_atual / 'assets/destino/01' / 'texto.txt'
+# Garantir que o diretório de destino exista
+# caminho_arquivo_destino.parent.mkdir(parents=True, exist_ok=True)
+# Copiar o arquivo
+shutil.copyfile(caminho_arquivo_origem, caminho_arquivo_destino)
 # Copiando arquivo com copy2
 
 # Movendo arquivos
