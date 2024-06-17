e80dcc371930c1560eff25e21326e63aff829195
leoCardosoDev <leocardosodev@gmail.com>
2024-06-17 15:06:21 -0300

    feat: delete specific folder

--- 05_copiando_e_movendo_pastas/04_deletando_pastas_vazias.py
+++ 05_copiando_e_movendo_pastas/04_deletando_pastas_vazias.py
@@ -1,3 +1,7 @@
 from pathlib import Path
 
 # Deletando pastas vazias
+pasta_atual = Path(__file__).parent / 'assets' / 'destino'
+pasta_remover = pasta_atual / '05' / '051'
+pasta_remover.rmdir()
+print(pasta_atual)
