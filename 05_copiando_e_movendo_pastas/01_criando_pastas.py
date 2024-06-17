5534c5acd0696c0753e56181d5e494712947ce14
leoCardosoDev <leocardosodev@gmail.com>
2024-06-17 14:45:33 -0300

    feat: create folders

--- 05_copiando_e_movendo_pastas/01_criando_pastas.py
+++ 05_copiando_e_movendo_pastas/01_criando_pastas.py
@@ -1,3 +1,7 @@
 from pathlib import Path
 
 # Criando pastas
+pasta_atual = Path(__file__).parent
+caminho_pasta_destino = pasta_atual / 'assets/destino/04'
+caminho_pasta_destino.mkdir(parents=True, exist_ok=True)
+print(pasta_atual)
