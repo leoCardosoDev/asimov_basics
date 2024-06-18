6cbfad621e8fa0a6dec25ed8899a6766b8dab02f
leoCardosoDev <leocardosodev@gmail.com>
2024-06-18 12:47:47 -0300

    feat: finalizando  desafio html

--- 03_arquivos/06_escrevendo_arquivo/03_desafio_html/00_modificando_arquivo_html.py
+++ 03_arquivos/06_escrevendo_arquivo/03_desafio_html/00_modificando_arquivo_html.py
@@ -4,3 +4,16 @@ item_remover = 'Passear com cachorro'
 pasta_atual = Path(__file__).parent
 with open(pasta_atual / 'view_lista.html') as html:
   linhas_html = html.readlines()
+nova_linha_html = []
+escrever_linha = True
+for i, linha in enumerate(linhas_html):
+  if i < len(linhas_html) - 3 and item_remover in linhas_html[i+2]:
+    escrever_linha = False
+  if escrever_linha:
+    nova_linha_html.append(linha)
+    print(i, linha)
+  if '</li>' in linha:
+    escrever_linha = True
+  
+with open(pasta_atual / 'view_lista_atualizada.html', mode='w') as novo_html:
+  novo_html.writelines(nova_linha_html)
