dfc800c11457a4425336f2fe3f99eae85aadf684
leoCardosoDev <leocardosodev@gmail.com>
2024-06-18 10:37:19 -0300

    feat: read multiples lines

---
+++ 03_arquivos/06_escrevendo_arquivo/04_lendo_todas_linhas.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+# Lendo linha a linha
+pasta_atual = Path(__file__).parent
+with open(pasta_atual / 'lista_de_compras.txt') as lista_compras:
+    print(lista_compras.readlines())
