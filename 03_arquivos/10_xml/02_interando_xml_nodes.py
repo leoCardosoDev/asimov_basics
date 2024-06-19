c0950a49046b04d33cf19c575a0da978b928364f
leoCardosoDev <leocardosodev@gmail.com>
2024-06-19 17:50:39 -0300

    refactor: add comments

--- 03_arquivos/10_xml/02_interando_xml_nodes.py
+++ 03_arquivos/10_xml/02_interando_xml_nodes.py
@@ -7,6 +7,7 @@ domtree = xml.dom.minidom.parse(str(pasta_atual / 'xmls' / 'livros.xml'))
 group = domtree.documentElement
 livros = domtree.getElementsByTagName('livro')
 
+# Interando pelos nodes xml
 for livro in livros:
   titulo = livro.getElementsByTagName('titulo')[0].childNodes[0].nodeValue
   autor = livro.getElementsByTagName('autor')[0].childNodes[0].nodeValue
