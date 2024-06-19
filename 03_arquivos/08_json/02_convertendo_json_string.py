f5670442a38ed98e18bdf9b5745bef479a54ae64
leoCardosoDev <leocardosodev@gmail.com>
2024-06-19 15:33:47 -0300

    feat: load file json

--- 03_arquivos/08_json/02_convertendo_json_string.py
+++ 03_arquivos/08_json/02_convertendo_json_string.py
@@ -1,36 +1,18 @@
 import json
 import sys
-
+from pathlib import Path
 
 sys.stdout.reconfigure(encoding='utf-8')
 sys.stdin.reconfigure(encoding='utf-8')
 sys.stderr.reconfigure(encoding='utf-8')
 
-data_json = '''
-{
-  "assinantes" : [
-    {
-      "nome": "Adriano Soares",
-      "telefone": "51 99999999",
-      "email": "adriano@email.com",
-      "em_dia": true
-    },
-    {
-      "nome": "Juliano faccioni",
-      "telefone": "51 99999999",
-      "email": "juliano@email.com",
-      "em_dia": false
-    }
-    ],
-  "data_extração": "2023/08/22"
-}
-'''
-
-print(type(data_json))
-dado_convertido = json.loads(data_json)
-print(type(dado_convertido))
-
-# Convertendo dicionario para json com identação
-dict_para_json = json.dumps(dado_convertido, ensure_ascii=False, indent=2)
-print(type(dict_para_json))
-print(dict_para_json)
+# Lendo arquivos Json
+pasta_atual = Path(__file__).parent
+with open(pasta_atual / 'jsons' / 'assinantes.json') as f:
+  dado_carregado = json.load(f)
+  print(type(dado_carregado))
+  print(dado_carregado)
+  print()
+  print(dado_carregado['assinantes'])
+  print()
+  print(dado_carregado['data_extração'])
