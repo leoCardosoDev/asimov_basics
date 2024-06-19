f6dc37ed9bdeedb8c9dcf5214a4c579534934ddd
leoCardosoDev <leocardosodev@gmail.com>
2024-06-19 17:40:46 -0300

    feat: interable xml files

---
+++ 03_arquivos/10_xml/02_interando_xml_nodes.py
@@ -0,0 +1,15 @@
+import xml.dom.minidom
+from pathlib import Path
+
+# Lendo Arquivo xml
+pasta_atual = Path(__file__).parent
+domtree = xml.dom.minidom.parse(str(pasta_atual / 'xmls' / 'livros.xml'))
+group = domtree.documentElement
+livros = domtree.getElementsByTagName('livro')
+
+for livro in livros:
+  titulo = livro.getElementsByTagName('titulo')[0].childNodes[0].nodeValue
+  autor = livro.getElementsByTagName('autor')[0].childNodes[0].nodeValue
+  preco = livro.getElementsByTagName('preco')[0].childNodes[0].nodeValue
+  print('Titulo:', titulo, '\nAutor:',  autor, '\nR$', preco)
+  print()
