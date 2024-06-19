5927db918643a2b352546c5160b3773bcf876106
leoCardosoDev <leocardosodev@gmail.com>
2024-06-19 14:47:32 -0300

    feat: challanger finish

--- 03_arquivos/07_planilhas/11_desafio_excel.py
+++ 03_arquivos/07_planilhas/11_desafio_excel.py
@@ -4,4 +4,9 @@ import pandas as pd
 pasta_atual = Path(__file__).parent
 tabela_clientes_dict = pd.read_excel(pasta_atual / 'clientes.xlsx', sheet_name=None)
 for nome_aba, tabela in tabela_clientes_dict.items():
-  tabela.to_excel(pasta_atual / 'planilhas' / 'planilhas_separadas' / f'{nome_aba.lower()}.xlsx')
+  tabela.to_excel(pasta_atual / 'planilhas' / 'planilhas_separadas' / f'{nome_aba.lower()}.xlsx', index=False)
+
+with pd.ExcelWriter(pasta_atual / 'planilhas' / 'planilha_consolidada' / 'consolidada.xlsx') as consolidada:
+  for arquivo in Path(pasta_atual / 'planilhas' / 'planilhas_separadas').glob('*.xlsx'):
+    tabela = pd.read_excel(arquivo)
+    tabela.to_excel(consolidada, sheet_name=arquivo.stem.upper(), index=False)
