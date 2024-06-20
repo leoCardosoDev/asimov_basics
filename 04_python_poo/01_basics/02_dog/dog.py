09f73314f89c9e13b67e1687055a4dc603eedf94
leoCardosoDev <leocardosodev@gmail.com>
2024-06-20 10:01:09 -0300

    feat: finalize class dog

--- 04_python_poo/01_basics/02_dog/dog.py
+++ 04_python_poo/01_basics/02_dog/dog.py
@@ -1,13 +1,19 @@
 class Dog:
-  def __init__(self, raca):
-    self.idade = 10
-    self.raca = raca
-    print(f'Cachorro {raca} instanciado')
+    def __init__(self, raca):
+        self.idade = 10
+        self.raca = raca
+        print(f'Cachorro {raca} instanciado')
+
+    def envelhecer(self):
+        self.idade += 1
+        return self.idade
+
 
 dog = Dog('Lab')
 dog2 = Dog('Chiuahua')
 
 dog.idade = 12
+dog2.envelhecer()
 
 print(dog.idade)
 print(dog.raca)
