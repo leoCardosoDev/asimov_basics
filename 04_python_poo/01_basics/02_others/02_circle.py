9876e6b631a110b14c1e56df5f43d0e3d43ce35d
leoCardosoDev <leocardosodev@gmail.com>
2024-06-20 10:41:06 -0300

    feat: add method return raio

--- 04_python_poo/01_basics/02_others/02_circle.py
+++ 04_python_poo/01_basics/02_others/02_circle.py
@@ -4,6 +4,9 @@ class Circle:
 
     def calcula_area(self):
         return self.raio * self.raio * 3.14
+    
+    def retorna_raio(self):
+        return self.raio
 
 
 circle1 = Circle()
@@ -15,3 +18,7 @@ print(circle2.raio)
 print('Area calculada:')
 print(circle1.calcula_area())
 print(circle2.calcula_area())
+
+print('Raio retornado')
+print(circle1.retorna_raio())
+print(circle2.retorna_raio())
