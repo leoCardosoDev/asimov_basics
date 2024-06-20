00448b3ff3626ecb25e4b7d5b0f0eb5150276ac1
leoCardosoDev <leocardosodev@gmail.com>
2024-06-20 09:55:29 -0300

    feat: add class dog and create new attributte

--- 04_python_poo/01_basics/02_dog/dog.py
+++ 04_python_poo/01_basics/02_dog/dog.py
@@ -1,9 +1,15 @@
 class Dog:
-  def __init__(self):
+  def __init__(self, raca):
     self.idade = 10
+    self.raca = raca
 
-dog = Dog()
-dog2 = Dog()
+dog = Dog('Lab')
+dog2 = Dog('Chiuahua')
+
 dog.idade = 12
+
 print(dog.idade)
 print(dog2.idade)
+
+print(dog.raca)
+print(dog2.raca)
