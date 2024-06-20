00f0050d0938b56beefa4e387599df0cf52b3e6f
leoCardosoDev <leocardosodev@gmail.com>
2024-06-20 10:36:36 -0300

    feat: create example class Circle calculate area

--- 04_python_poo/01_basics/02_others/02_circle.py
+++ 04_python_poo/01_basics/02_others/02_circle.py
@@ -2,9 +2,16 @@ class Circle:
     def __init__(self, raio=1):
         self.raio = raio
 
+    def clacula_area(self):
+        return self.raio * self.raio * 3.14
+
 
 circle1 = Circle()
 circle2 = Circle(2)
 print('Raio:')
 print(circle1.raio)
 print(circle2.raio)
+
+print('Area calculada:')
+print(circle1.clacula_area())
+print(circle2.clacula_area())
