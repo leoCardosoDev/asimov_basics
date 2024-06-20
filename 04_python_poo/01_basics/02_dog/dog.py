675c2b5ac54e83ef8af24bbbe07352ffa95f7444
leoCardosoDev <leocardosodev@gmail.com>
2024-06-20 09:57:46 -0300

    feat: add class dog and create new method

--- 04_python_poo/01_basics/02_dog/dog.py
+++ 04_python_poo/01_basics/02_dog/dog.py
@@ -2,6 +2,7 @@ class Dog:
   def __init__(self, raca):
     self.idade = 10
     self.raca = raca
+    print(f'Cachorro {raca} instanciado')
 
 dog = Dog('Lab')
 dog2 = Dog('Chiuahua')
@@ -9,7 +10,7 @@ dog2 = Dog('Chiuahua')
 dog.idade = 12
 
 print(dog.idade)
-print(dog2.idade)
-
 print(dog.raca)
+print()
+print(dog2.idade)
 print(dog2.raca)
