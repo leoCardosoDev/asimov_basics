df6cc4aeca13094f4717827602334ad65cd38861
leoCardosoDev <leocardosodev@gmail.com>
2024-06-21 15:33:22 -0300

    feat: add method listar_halters

--- 04_python_poo/02_projects/01_simulador_de_caos_academia/academia.py
+++ 04_python_poo/02_projects/01_simulador_de_caos_academia/academia.py
@@ -1,11 +1,14 @@
 import random
 
+
 class Academia:
-  def __init__(self):
-    self.halteres = [i for i in range(10,36) if i % 2 == 0]
-    self.porta_halteres = {}
-    self.reiniciar_o_dia()
-  
-  def reiniciar_o_dia(self):
-    self.porta_halteres = {i: i for i in self.halteres}
-  
+    def __init__(self):
+        self.halteres = [i for i in range(10, 36) if i % 2 == 0]
+        self.porta_halteres = {}
+        self.reiniciar_o_dia()
+
+    def reiniciar_o_dia(self):
+        self.porta_halteres = {i: i for i in self.halteres}
+
+    def listar_halteres(self):
+        return [i for i in self.porta_halteres.values() if i != 0]
