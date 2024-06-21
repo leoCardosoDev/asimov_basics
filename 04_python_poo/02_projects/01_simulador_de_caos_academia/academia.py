84c944cf133be2df5bb86227b4c135d74d4ccb11
leoCardosoDev <leocardosodev@gmail.com>
2024-06-21 18:07:52 -0300

    feat: mont carlo simulation

--- 04_python_poo/02_projects/01_simulador_de_caos_academia/academia.py
+++ 04_python_poo/02_projects/01_simulador_de_caos_academia/academia.py
@@ -13,6 +13,9 @@ class Academia:
     def listar_halteres(self):
         return [i for i in self.porta_halteres.values() if i != 0]
 
+    def listar_espacos(self):
+        return [i for i, j in self.porta_halteres.items() if j == 0]
+
     def pegar_haltere(self, peso):
         halt_pos = list(self.porta_halteres.values()).index(peso)
         key_halt = list(self.porta_halteres.keys())[halt_pos]
@@ -42,12 +45,36 @@ class Usuario:
         espacos = self.academia.listar_espacos()
         if self.tipo == 1:
             if self.peso in espacos:
-                self.academia.devolver_halter(self.peso, self.peso)
+                self.academia.devolver_haltere(self.peso, self.peso)
             else:
                 pos = random.choice(espacos)
-                self.academia.devolver_halter(pos. self.peso)
-
+                self.academia.devolver_haltere(pos, self.peso)
         if self.tipo == 2:
             pos = random.choice(espacos)
-            self.academia.devolver_halter(pos. self.peso)
+            self.academia.devolver_haltere(pos, self.peso)
         self.peso = 0
+
+
+academia = Academia()
+usuarios = [Usuario(1, academia) for i in range(10)]
+usuarios += [Usuario(2, academia) for i in range(1)]
+random.shuffle(usuarios)
+list_caos = []
+
+for k in range(50):
+    academia.reiniciar_o_dia()
+    for i in range(10):
+        random.shuffle(usuarios)
+        for user in usuarios:
+            user.iniciar_treino()
+        for user in usuarios:
+            user.finalizar_treino()
+    list_caos += [academia.calcular_caos()]
+
+print(academia.porta_halteres)
+print(academia.calcular_caos())
+print()
+print(list_caos)
+
+import seaborn as sns
+sns.displot(list_caos)
