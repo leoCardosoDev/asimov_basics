70bc016b21660052eb8bf82f03f7b5cd5110ac16
leoCardosoDev <leocardosodev@gmail.com>
2024-06-24 13:43:22 -0300

    feat: complete get_play_move method

--- 04_python_poo/02_projects/02_jogo_da_velha/tic_tac_toe.py
+++ 04_python_poo/02_projects/02_jogo_da_velha/tic_tac_toe.py
@@ -8,11 +8,14 @@ class TicTacToe:
 
     def print_board(self):
         print("")
-        print(" " + self.board[0][0] + " | " + self.board[0][1] + " | " + self.board[0][2])
+        print(" " + self.board[0][0] + " | " +
+              self.board[0][1] + " | " + self.board[0][2])
         print("___________")
-        print(" " + self.board[1][0] + " | " + self.board[1][1] + " | " + self.board[1][2])
+        print(" " + self.board[1][0] + " | " +
+              self.board[1][1] + " | " + self.board[1][2])
         print("___________")
-        print(" " + self.board[2][0] + " | " + self.board[2][1] + " | " + self.board[2][2])
+        print(" " + self.board[2][0] + " | " +
+              self.board[2][1] + " | " + self.board[2][2])
 
     def reset(self):
         self.board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
@@ -48,17 +51,34 @@ class TicTacToe:
                 if self.board[i][j] != " ":
                     c+1
                     break
-                
+
         if c == 0:
             self.done = 'd'
             print('Empate')
             return
-    
+
     def get_player_move(self):
-        pass
-    
+        invalid_mode = True
+        while invalid_mode:
+            try:
+                print('Digite a linha do seu próximo lance:')
+                x = int(input())
+                print('Digite a columna do seu próximo lance:')
+                y = int(input())
+                if x > 2 or x < 0 or y > 2 or y < 0:
+                    print('Coordenadas inválidas')
+                if self.board[x][y] != " ":
+                    print('Posição já preenchida')
+                    continue
+            except Exception as e:
+                print(e)
+                continue
+            invalid_mode = False
+            self.board[x][y] = 'X'
+
     def make_move(self):
         pass
+
 
 self = TicTacToe()
 self.print_board()
