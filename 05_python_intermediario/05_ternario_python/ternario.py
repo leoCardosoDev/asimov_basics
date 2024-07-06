c00ba467c144c1cb723345fa5f4de6246ea259a9
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-06 02:25:18 -0700

    feat: add example ternario

--- 05_python_intermediario/05_ternario_python/ternario.py
+++ 05_python_intermediario/05_ternario_python/ternario.py
@@ -6,3 +6,13 @@ else:
     maior_valor = y
 
 print(maior_valor)
+
+# Operador ternário em outras linguagens
+# var CONDIÇÃO ? valor se VERDADEIRO : valor se FALSO
+
+# Experssão condicional em Python
+# var = valor_verdadeiro SE condição SENÃO valor_falso
+x = 100
+valor_maior = x if x > y else y
+print(valor_maior)
+
