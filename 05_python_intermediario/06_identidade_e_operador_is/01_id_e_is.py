b74a4ac68ba2caec1243c5519dbc77a21b17b3b9
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-06 02:54:50 -0700

    feat: add example python with identy and operator is

--- 05_python_intermediario/06_identidade_e_operador_is/01_id_e_is.py
+++ 05_python_intermediario/06_identidade_e_operador_is/01_id_e_is.py
@@ -5,3 +5,27 @@ print(lista_a is lista_b)
 lista_a.append(6)
 print(lista_a == lista_b)
 
+bolo1 = {
+    'sabor': 'chocolate',
+    'tamanho': 'grande',
+    'preco': 50
+}
+
+bolo2 = {
+    'sabor': 'chocolate',
+    'tamanho': 'grande',
+    'preco': 50
+}
+print(bolo1 == bolo2)
+print(bolo1 is bolo2)
+bolo1['preco'] == 80
+print(bolo1 == bolo2)
+
+print(True is True)
+print(True is False)
+print(False is True)
+print(False is False)
+print(None is None)
+
+valor = 10
+print('Valor é nulo' if valor is None else 'Valor não é nulo')
