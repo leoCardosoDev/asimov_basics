0e06d3bdad942e0cf575c80ae1ceeb40d80771c4
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-06 02:33:56 -0700

    feat: add examples aditional

--- 05_python_intermediario/05_ternario_python/ternario.py
+++ 05_python_intermediario/05_ternario_python/ternario.py
@@ -16,3 +16,13 @@ x = 100
 valor_maior = x if x > y else y
 print(valor_maior)
 
+def pega_cor(valor):
+    return 'vermelho' if valor <= 0 else 'azul'
+
+for valor in [-1, 0, 1, 2]:
+    print(f'A cor do valor {valor} é {pega_cor(valor)}')
+
+numeros = [1,2,3,4]
+pares_quadrados = [ n** 2 if n % 2 == 0 else n for n in numeros]
+print(f'Números: {numeros}')
+print(f'Números ao quadrados de pares: {pares_quadrados}')
