8a3b4739bf7b239ee3c26378ebfb96636f43706f
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-06 00:13:00 -0700

    feat: add with bloc context

--- 05_python_intermediario/02_bloco_with/01_with.py
+++ 05_python_intermediario/02_bloco_with/01_with.py
@@ -4,3 +4,6 @@ arquivo.write('Esta é uma anotação especial!')
 arquivo.close()
 
 # com with
+with open('notas.txt', 'w') as arquivo2:
+    arquivo2.write('Nova linha de código')
+print('Arquivo foi fechado')
