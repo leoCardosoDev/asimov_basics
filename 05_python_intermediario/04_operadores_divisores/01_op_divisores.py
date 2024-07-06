64ff1b574db395e4896d0957a60fab3f45e124ac
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-06 00:58:38 -0700

    feat: add example one

--- 05_python_intermediario/04_operadores_divisores/01_op_divisores.py
+++ 05_python_intermediario/04_operadores_divisores/01_op_divisores.py
@@ -1,3 +1,13 @@
 print(10 / 3) # divisão padrão
 print(10 // 3) # divisão piso
 print(10 % 3) # Resto da divisão
+print()
+print('-'*50)
+print()
+segundos_totais = 65
+print(segundos_totais / 60)
+minutos = segundos_totais // 60
+print(minutos)
+segundos = segundos_totais % 60
+print(segundos)
+print(f'{minutos}m{segundos}s')
