1459a326f9619767371e043e325424ffd92122ef
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-09 15:53:47 -0700

    feat: itertools product

--- 05_python_intermediario/09_iteracoes_avancadas/03_itertools.py
+++ 05_python_intermediario/09_iteracoes_avancadas/03_itertools.py
@@ -3,13 +3,26 @@ import itertools
 # chain
 seq1 = (1, 2, 3)
 seq2 = ['a', 'b', 'c']
-for elemento in itertools.chain(seq1, seq2):
-    print(elemento)
+for sequencia in itertools.chain(seq1, seq2):
+    print(sequencia)
 
+print()
+print('***' * 50)
+print()
 # zip_longest
 nomes = ['Leo', 'Pri', 'Laura', 'Roberto', 'Guilherme']
 idades = [42, 34, 19, 47, 22]
 cpfs = ['xxx', 'yyy', 'zzz']
 emails = ['leo@gmail.com', 'pri@gmail.com']
-for elemento in itertools.zip_longest(nomes, idades, cpfs, emails, fillvalue='???'):
-    print(elemento)
+for lista in itertools.zip_longest(nomes, idades, cpfs, emails, fillvalue='???'):
+    print(lista)
+
+print()
+print('***' * 50)
+print()
+# product
+comidas = ['Churrasco', 'Pizza', 'Sushi']
+bebidas = ('Refrigerante', 'Água')
+for prato in itertools.product(comidas, bebidas):
+    print(prato)
+
