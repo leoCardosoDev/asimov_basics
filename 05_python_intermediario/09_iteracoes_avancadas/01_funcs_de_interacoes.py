e42044c6f206d96a07806feffb9a7cf6103b8fc3
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-08 18:43:46 -0700

    feat: add sorted concept

--- 05_python_intermediario/09_iteracoes_avancadas/01_funcs_de_interacoes.py
+++ 05_python_intermediario/09_iteracoes_avancadas/01_funcs_de_interacoes.py
@@ -3,3 +3,10 @@ nomes = ['Juliano', 'José', 'Lucas', 'Luiza']
 for i, nome in enumerate(nomes, 1):
     print(f'Indice: {i} -> Nome: {nome}')
 
+# Sorted
+conjunto = [10, 4, 0, 5, 9, 1, -1, -2]
+ordenados = sorted(conjunto)
+ordenados_reversed = sorted(conjunto, reverse=True)
+print(f'Conjunto: {conjunto}\nOrdenados: {ordenados}')
+print()
+print(f'Ordenados: {ordenados}\nOrdenados reverso: {ordenados_reversed}')
