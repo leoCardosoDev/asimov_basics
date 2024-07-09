44cda736e8bba6be2ed567517f19f5e3a6392472
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-09 15:46:20 -0700

    feat: itertools zip longest None

--- 05_python_intermediario/09_iteracoes_avancadas/03_itertools.py
+++ 05_python_intermediario/09_iteracoes_avancadas/03_itertools.py
@@ -5,3 +5,11 @@ seq1 = (1, 2, 3)
 seq2 = ['a', 'b', 'c']
 for elemento in itertools.chain(seq1, seq2):
     print(elemento)
+
+# zip_longest
+nomes = ['Leo', 'Pri', 'Laura', 'Roberto', 'Guilherme']
+idades = [42, 34, 19, 47, 22]
+cpfs = ['xxx', 'yyy', 'zzz']
+emails = ['leo@gmail.com', 'pri@gmail.com']
+for elemento in itertools.zip_longest(nomes, idades, cpfs, emails):
+    print(elemento)
