c534f6129a1dc150e4aa3509922625d85e06fcf4
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-10 15:30:04 -0700

    feat: create this all

--- 05_python_intermediario/10_funcoes/02_built-in_relevantes.py
+++ 05_python_intermediario/10_funcoes/02_built-in_relevantes.py
@@ -4,3 +4,7 @@ if isinstance(valor1, (int, float)):
     print('Valor é do tipo númerico')
 else:
     print('Valor não é do tipo númerico')
+
+# any e all
+booleanos = [True, False, True]
+print(all(booleanos))
