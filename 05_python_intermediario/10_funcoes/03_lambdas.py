b1fe2787ca0cfee23a1d4e6ec63f3fa73973afa0
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-10 16:25:58 -0700

    feat: add another example lambda

--- 05_python_intermediario/10_funcoes/03_lambdas.py
+++ 05_python_intermediario/10_funcoes/03_lambdas.py
@@ -10,3 +10,9 @@ print(list(filtro))
 # Sintaxe da função lambda
 # lambda <ARGUMENTO DA FUNÇÃO>: <LÓGICA DA FUNÇÃO>
 # lambda x, y, z: x + y + z
+
+# another example
+mapa = map(lambda x: str(x+2), [1, 2, 3])
+print(list(mapa))
+
+print(list(map(lambda x: str(x+2), [1, 2, 3])))
