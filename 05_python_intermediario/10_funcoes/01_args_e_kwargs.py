9c7c61a01d6a7c245cb54e966b40dee979cc4adb
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-10 11:22:25 -0700

    feat: example descompact var

--- 05_python_intermediario/10_funcoes/01_args_e_kwargs.py
+++ 05_python_intermediario/10_funcoes/01_args_e_kwargs.py
@@ -12,3 +12,13 @@ def exibir_argumentos(*args, **kwargs):
     print(f'Argumentos passados SEM palavra-chave: {args}')
     print(f'Argumentos passados COM palavra-chave: {kwargs}')
 exibir_argumentos('a,', 'b', 'c', 1, 2, 3, a='A', b='B', c='C')
+
+# Desempacotando argumentos
+valores = [2, 4, 6, 8]
+dicionario = { 'nome': 'Leonardo', 'idade': 42, 'cpf': 'xxx.xxx.xxx-xx'}
+def desempactando_variaveis(*args, **kwargs):
+    print('---'*30)
+    print(args)
+    print(kwargs)
+    print('---'*30)
+desempactando_variaveis(*valores, **dicionario)
