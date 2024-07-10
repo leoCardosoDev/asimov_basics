774c9b511eade4c36f891fe65256aca2851357d2
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-10 11:11:12 -0700

    feat: add convention args and kwargs

--- 05_python_intermediario/10_funcoes/01_args_e_kwargs.py
+++ 05_python_intermediario/10_funcoes/01_args_e_kwargs.py
@@ -6,3 +6,9 @@ print(somar_fixo(1,2,3))
 def somar_com_args(*valores, **valores2):
     return sum(valores)
 print(somar_com_args(1,2,3,4,5, x=6, y=7))
+
+# Convenção
+def exibir_argumentos(*args, **kwargs):
+    print(f'Argumentos passados SEM palavra-chave: {args}')
+    print(f'Argumentos passados COM palavra-chave: {kwargs}')
+exibir_argumentos('a,', 'b', 'c', 1, 2, 3, a='A', b='B', c='C')
