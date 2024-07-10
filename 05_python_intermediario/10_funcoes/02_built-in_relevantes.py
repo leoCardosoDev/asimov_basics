bbb3837d7f90a85331bb00dd3a459a5e41dbd13d
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-10 15:41:14 -0700

    feat: int valores

--- 05_python_intermediario/10_funcoes/02_built-in_relevantes.py
+++ 05_python_intermediario/10_funcoes/02_built-in_relevantes.py
@@ -9,3 +9,8 @@ else:
 booleanos = [True, False, True]
 print(all(booleanos))
 print(any(booleanos))
+valores = [1, 2, 2.5, 3]
+if all(isinstance(valor, int) for valor in valores):
+    print('Todos os valores são int')
+else:
+    print('Nem todos os valores são int')
