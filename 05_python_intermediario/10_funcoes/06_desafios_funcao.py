ce8686d86016fd37df25a7f27cb492a9e15fd116
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-11 16:48:15 -0700

    feat: challanger 2

--- 05_python_intermediario/10_funcoes/06_desafios_funcao.py
+++ 05_python_intermediario/10_funcoes/06_desafios_funcao.py
@@ -9,3 +9,15 @@ valores_maiores_que_tres = filter(lambda x: x > 3, valores)
 quadrados = map(lambda x: x ** 2, valores_maiores_que_tres)
 quadrados_maiores_que_tres_map_filter = list(quadrados)
 print("map e filter:", quadrados_maiores_que_tres_map_filter)
+
+import functools
+def mult(a, b):
+    return a * b
+
+def multiplicar_por(n):
+    return functools.partial(mult, b=n)
+
+dobrar = multiplicar_por(2)
+print(dobrar(3))
+vezes_cinco = multiplicar_por(5)
+print(vezes_cinco(10))
