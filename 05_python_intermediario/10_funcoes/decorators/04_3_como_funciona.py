cef7edc09ee91cb3de3ccdc4d0ced0940bbdb081
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-11 14:49:16 -0700

    feat: add decorators new

--- 05_python_intermediario/10_funcoes/decorators/04_3_como_funciona.py
+++ 05_python_intermediario/10_funcoes/decorators/04_3_como_funciona.py
@@ -1,6 +1,15 @@
 # Como decoradores funcionam?
+# Criando um decorador
+def meu_decorador(func):
+    def meu_pacote(*args, **kwargs):
+        retorno = func(*args, **kwargs)
+        return retorno.upper()
+    return meu_pacote
+
 def dizer_oi(nome):
     return f'Olá {nome}!'
 
-resultado = dizer_oi('Leo')
-print(resultado)
+dizer_oi = meu_decorador(func=dizer_oi) # mesma cois que fazer @meu_decorador
+print(dizer_oi('Pri Cardoso'))
+
+
