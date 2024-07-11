b802c2028cbcd77ba37f5e7f7b3c63c0c31f614c
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-11 13:09:24 -0700

    feat: add func exibe func

--- 05_python_intermediario/10_funcoes/04_usando_decorators.py
+++ 05_python_intermediario/10_funcoes/04_usando_decorators.py
@@ -5,3 +5,10 @@ def funcao():
 minha_funcao = funcao
 retorno = minha_funcao()
 print(retorno)
+
+def exibe_funcao(f):
+    print(f'Objeto de função recebido: {f}')
+    print(f'Nome da função: {f.__name__}')
+
+exibe_funcao(funcao)
+print('**'*40)
